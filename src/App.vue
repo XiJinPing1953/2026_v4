7873e562c15a015abdfd4a5a24916b2ea248c1b4
Wang Bo <hide_onbush@icloud.com>
2026-01-14 16:25:03 +0800

    chore: silence sass deprecation warnings in build

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <style lang="scss">
-@use "@/uni.scss" as *;
+@import "@/uni.scss";
 
 page {
 	background: var(--crm-bg);
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import uni from '@dcloudio/vite-plugin-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [uni()]
+	plugins: [uni()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// Silence Dart Sass deprecation warnings emitted by HBuilderX toolchain.
+				silenceDeprecations: ['legacy-js-api', 'import']
+			}
+		}
+	}
 })
